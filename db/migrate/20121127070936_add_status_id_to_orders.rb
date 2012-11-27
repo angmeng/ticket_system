754ac8b4c7a3dd70fdda1edbb8cea4c3c76deb0a
Jaz Lim <angmeng@gmail.com>
2012-11-27 16:35:20 +0800

    Adjust orders layout

--- db/migrate/20121127070936_add_status_id_to_orders.rb
+++ db/migrate/20121127070936_add_status_id_to_orders.rb
@@ -1,8 +1,8 @@
 class AddStatusIdToOrders < ActiveRecord::Migration
   def change
-    # add_column :orders, :status_id, :integer, :default => 0
-    # add_index :orders, :status_id
-    # remove_column :tickets, :status_id
+    add_column :orders, :status_id, :integer, :default => 0
+    add_index :orders, :status_id
+    remove_column :tickets, :status_id
     # remove_index  :tickets, :status_id
   end
 end
